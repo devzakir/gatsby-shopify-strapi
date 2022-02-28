ac86fece09f8fef8a6fbd94734ff5759366e9ebe
Zakir Hossen <web.zakirbd@gmail.com>
2022-02-28 11:59:30 +0600

    feat: shopify gatsby website added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,37 @@
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
       title: `new`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
-    resolve: 'gatsby-source-shopify',
-    options: {
-      "shopName": "zakirshop20",
-      "accessToken": "2740f14865b0438f6d49e3dea9eda8b5"
-    }
-  }, "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: 'gatsby-source-shopify',
+      options: {
+        storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
+        apiKey: process.env.SHOPIFY_API_KEY,
+        password: process.env.SHOPIFY_SHOP_PASSWORD,
+        shopifyConnections: ["collections"],
+      }
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap", {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    }, 
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp", {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }
+  ]
 };
